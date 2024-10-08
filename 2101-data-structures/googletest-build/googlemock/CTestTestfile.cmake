d644616a7eb7529565c8eff88b38902f167d1732
Laniakea <112793967+abilliontrillionstars@users.noreply.github.com>
2024-10-08 14:52:02 -0400

    purge googletest from chromebook

--- 2101-data-structures/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Ethan Ward/OneDrive/Documents/c-programs/2101-data-structures/googletest-main/googlemock
-# Build directory: C:/Users/Ethan Ward/OneDrive/Documents/c-programs/2101-data-structures/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- 2101-data-structures/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Ethan Ward/OneDrive/Documents/c-programs/2101-data-structures/googletest-main/googletest
-# Build directory: C:/Users/Ethan Ward/OneDrive/Documents/c-programs/2101-data-structures/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
